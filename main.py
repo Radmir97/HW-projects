471e5c518080a227489413070cf8c82b2b052833
Radmir97 <96171820+Radmir97@users.noreply.github.com>
2021-12-15 11:35:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,75 @@
-import math
+import random
 
 
-
-class Car:
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
-        self.direction = direction
-        self.distance = 0
+class KillError(Exception):
+    pass
 
 
-    def rotate(self, angle):
-        self.direction += angle
-        self.direction = self.direction % 360
-
-    def move(self, distance):
-        direction_rad = math.radians(self.direction)
-        self.x += distance * math.cos(direction_rad)
-        self.y += distance * math.sin(direction_rad)
-
-    def __str__(self):
-        lines = f'Транспортное средство находится в координатах:'
-        f'x : {round(self.x, 2)}'
-        f'y : {round(self.y, 2)}'
-        f'направление : {round(self.direction, 2)}'
-        f'пройденное расстояние : {round(self.distance, 2)}'
-        return ('\n').join(lines)
+class DrunkError(Exception):
+    pass
 
 
-class Bus(Car):
-    def __init__(self, x, y, direction):
-        super().__init__(x, y, direction)
-        self.passengers = 0
-        self.money = 0
+class CarCrashError(Exception):
+    pass
 
-    def move(self,distance):
-        self.money += 0.01 * self.passengers * distance
-        super().move(distance)
 
-    def enter(self, passengers):
-        if passengers + self.passengers > 60:
-            print('Достигнута максимальная вместимость автобуса'
-                  'Уехать смогли только {:d}'.format(60 - self.passengers),
-                  'Остались {:d}'.format(passengers - (60 - self.passengers)))
-            passengers = 60 - passengers
-        return passengers
+class GluttonyError(Exception):
+    pass
 
-    def exit(self, passengers):
-        if passengers > self.passengers:
-            print('Все вышли из автобуса')
-            passengers = self.passengers
-        return passengers
 
-    def __str__(self):
-        lines = [super().__str__(),
-            f'В автобусе {self.passengers} пассажиров',
-            f'У водителя {round(self.money, 2)} денег',]
-        return ('\n').join(lines)
+class DepressionError(Exception):
+    pass
+
+
+def one_day(start_karma, goal):
+    while start_karma < goal:
+        chance = random.randint(1, 8)
+        fail = random.randint(1, 6)
+        if fail == 1:
+            with open('karma.log.txt', 'a') as log:
+                log.write('KillError')
+                log.write('\n')
+            try:
+                pass
+            except:
+                raise KillError('KillError')
+        if fail == 2:
+            with open('karma.log.txt', 'a') as log:
+                log.write('DrunkError')
+                log.write('\n')
+            try:
+                pass
+            except:
+                raise DrunkError('DrunkError')
+        if fail == 3:
+            with open('karma.log.txt', 'a') as log:
+                log.write('CarCrashError')
+                log.write('\n')
+            try:
+                pass
+            except:
+                raise CarCrashError('CarCrashError')
+        if fail == 4:
+            with open('karma.log.txt', 'a') as log:
+                log.write('GluttonyError')
+                log.write('\n')
+            try:
+                pass
+            except:
+                raise GluttonyError('GluttonyError')
+        if fail == 5:
+            with open('karma.log.txt', 'a') as log:
+                log.write('DepressionError')
+                log.write('\n')
+            try:
+                pass
+            except:
+                raise DepressionError('DepressionError')
+        start_karma += chance
+        if start_karma >= goal:
+            print(start_karma, 'очков кармы')
+
+
+one_day(0, 500)
+
+# зачтено
